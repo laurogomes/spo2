ff3da8ec3a6999e558401bbd98416e5db18e13d2
laurogomes <lbg42@hotmail.com>
2017-06-03 06:37:05 +0900

    Create spo2.js

---
+++ spo2.js
@@ -0,0 +1,21 @@
+<script src="https://content.jwplatform.com/libraries/zkpZNdwa.js"></script>
+<div id='playerBLiyUVVsMPRD'></div>
+<script type="text/javascript">
+    var player = jwplayer('playerBLiyUVVsMPRD');
+        jwplayer("playerBLiyUVVsMPRD").setup({
+            file: "http://painel.tvdigitalsbl.net:8081/live/iva01/0101/1404.m3u8",
+            image: "../../img/sportv.png",
+            autostart: true,
+            hlshtml: true,
+            width: '640',
+            height: '360',
+            logo: {
+            },
+            skin: {
+                name: "seven"
+            }
+        });
+    </script>
+
+
+<script src="https://apis.google.com/js/platform.js"></script>
